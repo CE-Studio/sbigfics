fdfe898da04b414b8c01505becd1668a09452fe1
EpsilonTheDerg <jamesclamey@gmail.com>
2023-07-30 20:33:10 -0400

    no message

---
+++ assets/gamescripts/gapspillar.gd
@@ -0,0 +1,56 @@
+extends AnimatableBody2D
+
+
+const ORIGIN_Y := 512
+const EXTENT := 350
+const START_CENTER_BUFFER := 64
+const ADVANCE_BUFFER := 0.05
+const X_SPACE := 200
+const SMEAR_START_X := 480
+const SCORE_SCALE := 1.5
+
+var _advanceTime := 0.0
+
+@onready var _area:Area2D = $"Area2D"
+@onready var _pillar2:AnimatableBody2D = $"../Pillar2"
+@onready var _pillar3:AnimatableBody2D = $"../Pillar3"
+@onready var _pillarParent:Node2D = $".."
+@onready var _smear:Sprite2D = $"../../Smear"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    position = Vector2(position.x, _get_rand_pos(true))
+    _pillar2.position = Vector2(position.x + X_SPACE, _get_rand_pos(false))
+    _pillar3.position = Vector2(position.x + X_SPACE * 2, _get_rand_pos(false))
+    _smear.lerpY = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+    if _advanceTime > 0.0:
+        _advanceTime -= delta
+        if _advanceTime <= 0.0:
+            _area.set_deferred("monitoring", true)
+    
+    
+func _get_rand_pos(considerBuffer:bool):
+    var output
+    if (considerBuffer):
+        output = randi_range(START_CENTER_BUFFER, EXTENT)
+        output *= 1 if randf() > 0.5 else -1
+    else:
+        output = randi_range(-EXTENT, EXTENT)
+    return ORIGIN_Y + output
+
+
+func _on_area_2d_body_entered(body):
+    if _advanceTime <= 0.0:
+        _smear.position = Vector2(SMEAR_START_X, position.y)
+        position = Vector2(position.x, _pillar2.position.y)
+        _pillar2.position = Vector2(_pillar2.position.x, _pillar3.position.y)
+        _pillar3.position = Vector2(_pillar3.position.x, _get_rand_pos(false))
+        _area.set_deferred("monitoring", false)
+        _advanceTime = ADVANCE_BUFFER
+        _pillarParent.position += Vector2(X_SPACE, 0.0)
+        Game.score += 5.0
---
+++ assets/gamescripts/gapsplayer.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+
+
+var _vel := 0.0
+var _lastSign := 0
+var _lerpPoint := 0.0
+const MAX_VEL := 400.0
+const ACCEL := 750.0
+const OFFSET := 110
+const BOUNDS := 45.0
+const SCORE_SCALE := 1.5
+const ROT_MULT := 0.0025
+const FIELD_SIZE := 804
+const TURNAROUND := 2.5
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+    _lerpPoint = inverse_lerp(-MAX_VEL, MAX_VEL, _vel)
+    if _vel != 0.0:
+        _lastSign = 1 if _vel > 0.0 else -1
+    else:
+        _lastSign = 0
+    if Input.get_action_strength("MouseR") > 0.9:
+        _vel += ACCEL * delta * lerpf(TURNAROUND, 1, _lerpPoint)
+    elif Input.get_action_strength("MouseL") > 0.9:
+        _vel -= ACCEL * delta * lerpf(1, TURNAROUND, _lerpPoint)
+    else:
+        _vel -= ACCEL * delta * _lastSign
+        if ((_vel > 0.0 && _lastSign == -1) || (_vel < 0.0 && _lastSign == 1)):
+            _vel = 0.0
+            _lastSign = 0
+    _vel = clampf(_vel, -MAX_VEL, MAX_VEL)
+    velocity = Vector2(0, _vel)
+    move_and_slide()
+    position = Vector2(position.x, OFFSET + clampf(position.y - OFFSET, BOUNDS, FIELD_SIZE - BOUNDS))
+    rotation = _vel * ROT_MULT
